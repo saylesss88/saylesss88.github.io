c45036fe6859c3fe8e52210b0263455477a6ea7d
saylesss88 <saylesss87@proton.me>
2025-11-26 09:02:10 -0500

    Auto descriptions

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f8f36cb9.js',
+                path_to_root + 'searchindex-35736064.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
