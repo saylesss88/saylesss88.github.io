3ebf965de1e6e98f33d063c9b696d3df32e90c8d
saylesss88 <saylesss87@proton.me>
2025-12-09 11:12:58 -0500

    require-sig

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8c9cb75f.js',
+                path_to_root + 'searchindex-28693fca.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
