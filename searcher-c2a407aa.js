23b18ae63c69b485d49281d3e93c7a8c835dc6e2
saylesss88 <saylesss87@proton.me>
2025-12-23 07:57:38 -0500

    Brave

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-60b5bb13.js',
+                path_to_root + 'searchindex-739ad4c4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
