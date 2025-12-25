1c6323285a1d5e58b5011778a5e232c17d1b2c1c
saylesss88 <saylesss87@proton.me>
2025-12-25 17:40:53 -0500

    dnscrypt-proxy ODoH

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-739ad4c4.js',
+                path_to_root + 'searchindex-0b5ab5ab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
