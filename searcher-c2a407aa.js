ea7878c1836f36e65fc953f45b4765fd0230281a
saylesss88 <saylesss87@proton.me>
2025-12-30 17:48:59 -0500

    Blocklists

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9c0975e6.js',
+                path_to_root + 'searchindex-bad7f693.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
