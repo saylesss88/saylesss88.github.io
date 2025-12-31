e1be12d87aa9505ac53f026e3e222b086f69c51a
saylesss88 <saylesss87@proton.me>
2025-12-31 18:26:06 -0500

    missing comma in rust package chapter

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-76ad8a42.js',
+                path_to_root + 'searchindex-fdaf0b63.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
