c263272f9b32afaad7468fda769d0b4e5083167e
saylesss88 <saylesss87@proton.me>
2025-12-31 13:42:11 -0500

    syntax error

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-217d9aab.js',
+                path_to_root + 'searchindex-933d4640.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
