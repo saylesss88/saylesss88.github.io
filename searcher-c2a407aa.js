fb67f3236d59cec44b0b2eca6a3b683a3421403d
saylesss88 <saylesss87@proton.me>
2025-12-31 15:17:58 -0500

    Local nixpkgs

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-842b556c.js',
+                path_to_root + 'searchindex-76ad8a42.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
