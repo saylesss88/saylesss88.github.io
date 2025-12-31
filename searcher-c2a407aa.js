cdbb9932b28ac5d7a844be731b202c53b2bb2034
saylesss88 <saylesss87@proton.me>
2025-12-31 10:17:27 -0500

    crate to nix added to SUMMARY

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1ef44e32.js',
+                path_to_root + 'searchindex-2e1467d8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
