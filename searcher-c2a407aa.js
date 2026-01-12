55fce19f4834bb0ac46d42e5584b9f5ba196fd72
saylesss88 <saylesss87@proton.me>
2026-01-12 17:53:13 -0500

    catppuccin theme change

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-edd9eb45.js',
+                path_to_root + 'searchindex-83b6c2a8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
