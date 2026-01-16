b76a77039c8f6fe708b0911d33fd5d61d60da84b
saylesss88 <saylesss87@proton.me>
2026-01-16 09:54:52 -0500

    format disk, not partitions

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0bd220e9.js',
+                path_to_root + 'searchindex-7b105472.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
