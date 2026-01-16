05b25c179a5158dcd9d628680638a54e4bf9bb0a
saylesss88 <saylesss87@proton.me>
2026-01-16 08:38:37 -0500

    ZFS links

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-edfedce8.js',
+                path_to_root + 'searchindex-f7373162.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
