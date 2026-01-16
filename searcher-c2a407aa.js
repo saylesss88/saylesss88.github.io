a6c2f1c5ad66f684f284b458ff70bc578d6ddfdd
saylesss88 <saylesss87@proton.me>
2026-01-16 12:25:40 -0500

    persisting SSH keys

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-06e0de8e.js',
+                path_to_root + 'searchindex-ffd71e9f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
