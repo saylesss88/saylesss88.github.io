048846ee5267b29f7053e60e04f86f7b5e58dd03
saylesss88 <saylesss87@proton.me>
2026-01-16 10:10:57 -0500

    add: link to flakey repo

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7b105472.js',
+                path_to_root + 'searchindex-06e0de8e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
