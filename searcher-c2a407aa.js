54d30d17084463acd95d3bd3b60ef8deb301a5ef
saylesss88 <saylesss87@proton.me>
2026-01-17 17:57:29 -0500

    openzfs-docs

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7b240351.js',
+                path_to_root + 'searchindex-a76d316a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
