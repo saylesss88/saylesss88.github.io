e7b27be3e7771e0cf5730881f7a9d63e966afbe6
saylesss88 <saylesss87@proton.me>
2026-01-18 07:15:53 -0500

    formKC

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a76d316a.js',
+                path_to_root + 'searchindex-cff209f8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
