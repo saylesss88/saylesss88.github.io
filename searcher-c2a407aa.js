d6cbd12399a443d76c1562f0e715b33c90eac7a4
saylesss88 <saylesss87@proton.me>
2026-01-18 11:42:42 -0500

    edit encrypted_zfs chapter, use postMountCommands over postResumeCommands

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d19a0eee.js',
+                path_to_root + 'searchindex-6a2a1afe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
