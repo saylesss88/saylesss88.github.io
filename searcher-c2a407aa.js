00a636915f06e48974c0752a1cc72b9c3e71005e
saylesss88 <saylesss87@proton.me>
2026-01-18 13:10:16 -0500

    ZFS vs LUKS

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7ed6ecfe.js',
+                path_to_root + 'searchindex-2cac2d64.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
