4700fc697e9f8b26c0d95997e4ef08e7ab346e10
saylesss88 <saylesss87@proton.me>
2026-01-18 15:14:28 -0500

    starter repo

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1897efae.js',
+                path_to_root + 'searchindex-0b75640c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
