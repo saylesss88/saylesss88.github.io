1a818c1d6d8aee0d15f3adff13db083a7f59d680
saylesss88 <saylesss87@proton.me>
2026-01-19 14:09:14 -0500

    lanzaboote current version

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-95474823.js',
+                path_to_root + 'searchindex-d24981d7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
