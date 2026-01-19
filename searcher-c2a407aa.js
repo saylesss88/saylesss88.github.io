62a760b29388905d6c85a9d04b91f209a1e8bc55
saylesss88 <saylesss87@proton.me>
2026-01-19 18:51:45 -0500

    Recovery

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bd8a562d.js',
+                path_to_root + 'searchindex-b6adffc7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
