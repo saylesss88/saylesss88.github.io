03d471f8d8947fada3c0f010886832f27cbeb580
saylesss88 <saylesss87@proton.me>
2026-01-20 09:19:11 -0500

    KVM chapter internal vs external snapshots

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4ac9ee02.js',
+                path_to_root + 'searchindex-c2e768eb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
