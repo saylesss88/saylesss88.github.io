aac60ba0f307e001db544fcdb73b5a9640c84b4b
saylesss88 <saylesss87@proton.me>
2026-01-21 11:20:05 -0500

    dnscrypt-proxy

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8e31dfae.js',
+                path_to_root + 'searchindex-54e4b5a7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
