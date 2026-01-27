4a393447ff34bc026ed5cfd2f1667fa3daf23ff8
saylesss88 <saylesss87@proton.me>
2026-01-27 07:10:06 -0500

    feat: refactor SUMMARY.md

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-29a79acc.js',
+                path_to_root + 'searchindex-03f56740.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
