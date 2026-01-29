e2726b3f0efe8b31be9083fedf468132d256ddda
saylesss88 <saylesss88@users.noreply.github.com>
2026-01-29 14:46:43 +0000

    Deploy nix-book: 9dacc8ccf534176e8bb2bddaba11f2501b40a820 saylesss88/nix-book@9dacc8ccf534176e8bb2bddaba11f2501b40a820

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fa93834f.js',
+                path_to_root + 'searchindex-900be3b9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
