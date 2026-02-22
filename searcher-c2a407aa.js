2711af79361f5497cc0d534f7e92ef9c4146e4a3
saylesss88 <saylesss87@proton.me>
2026-02-22 14:10:41 -0500

    ACLs

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d2014188.js',
+                path_to_root + 'searchindex-16b7c73d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
