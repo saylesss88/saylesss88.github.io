fc9c811af369aac0fbcfcec3a15b5877eb3146c3
saylesss88 <saylesss87@proton.me>
2026-03-01 23:30:13 +0000

    zfs bare metal

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-16b7c73d.js',
+                path_to_root + 'searchindex-2a398fe6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
