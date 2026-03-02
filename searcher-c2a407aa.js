f0a0bd476d3b7e7f71beaace36b19abbbe6d4448
saylesss88 <saylesss87@proton.me>
2026-03-02 00:07:36 +0000

    edits

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2a398fe6.js',
+                path_to_root + 'searchindex-91324deb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
