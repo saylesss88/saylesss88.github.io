7717c5c2165e2f0441fe06b97405d6b164aa17cd
saylesss88 <saylesss87@proton.me>
2026-03-02 16:21:02 +0000

    edits to zfs bare metal chapter

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-91324deb.js',
+                path_to_root + 'searchindex-de2fbd81.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
