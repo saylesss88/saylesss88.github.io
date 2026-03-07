36af59d0ddc6d0c330f11911012e5767302b3c0a
saylesss88 <saylesss87@proton.me>
2026-03-07 17:08:12 -0500

    edit dendritic chapter

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5f05eb64.js',
+                path_to_root + 'searchindex-de906b5b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
