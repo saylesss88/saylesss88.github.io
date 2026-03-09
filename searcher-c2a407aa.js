36247b4589992e795f7f0dc5606567a23eb928cb
saylesss88 <saylesss87@proton.me>
2026-03-08 20:23:41 -0400

    add modular flake-parts section

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-de906b5b.js',
+                path_to_root + 'searchindex-047d2e73.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
