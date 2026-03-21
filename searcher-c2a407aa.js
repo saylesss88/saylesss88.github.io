7a8b3259b701e19da732d3e150b6c54a76635fa0
saylesss88 <saylesss87@proton.me>
2026-03-21 19:56:44 -0400

    practical jj additions

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e15de5ec.js',
+                path_to_root + 'searchindex-176046b3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
