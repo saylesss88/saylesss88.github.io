c65c91153f0b3ff82764448e53729283b7207282
saylesss88 <saylesss87@proton.me>
2026-03-24 18:41:31 -0400

    jj rebase

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e0da5bff.js',
+                path_to_root + 'searchindex-61d180d4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
