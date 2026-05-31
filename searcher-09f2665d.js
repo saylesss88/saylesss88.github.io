3fa91f666e61560d614f09d49253b368d1654d92
saylesss88 <saylesss88@users.noreply.github.com>
2026-05-31 11:17:56 +0000

    Deploy nix-book: f479d568b0848a69d0247a28b7c4a4064f09c0aa saylesss88/nix-book@f479d568b0848a69d0247a28b7c4a4064f09c0aa

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3b809b83.js',
+                path_to_root + 'searchindex-92f413db.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
