9a1762969584f774a652149558385c88bc9ba129
saylesss88 <saylesss87@proton.me>
2026-07-17 12:33:47 -0400

    testing mdbook-draw

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b722661e.js',
+                path_to_root + 'searchindex-3a9d424a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
